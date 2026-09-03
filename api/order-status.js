790605494288c45459f57a0c994e09a32f52806c
pranjal030703 <pranjaldhardubey@gmail.com>
2026-09-03 17:32:42 +0530

    fixed Update order-status.js

--- api/order-status.js
+++ api/order-status.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Please use POST method' });
   }
 
-  const { order_number } = req.body || {};
+  const { order_number } = req.body?.arguments || {};
 
   if (!order_number) {
     return res.status(400).json({ error: 'order_number is required' });
